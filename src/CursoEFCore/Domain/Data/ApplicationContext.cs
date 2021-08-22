825f3722e6aac7194c37652cda79c50f304bc060
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-08-22 12:08:47 -0300

    Criando classes de entidades

---
+++ src/CursoEFCore/Domain/Data/ApplicationContext.cs
@@ -0,0 +1,23 @@
+using System;
+using CursoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace CursoEFCore.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string connectionString = "Data source=(localdb)\\mssqllocaldb; Initial Catalog=C002; Integrated Security=true;polling=true";
+
+            optionsBuilder
+                .UseSqlServer(connectionString)
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+    }
+}
---
+++ src/CursoEFCore/Domain/Funcionario.cs
@@ -0,0 +1,12 @@
+namespace CursoEFCore.Domain
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        
+        public int DepatamentoId { get; set; }
+        public Departamento Departamento { get; set; }
+    }
+}
