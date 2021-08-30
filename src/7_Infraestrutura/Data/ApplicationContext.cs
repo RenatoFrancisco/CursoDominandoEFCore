4f71ab665902ec3b1225cf68bf5553cdbc282ad0
renatofrancisco.s <renatofrancisco.s@gmail.com>
2021-08-29 23:03:00 -0300

    Configurando log simplificado

---
+++ src/7_Infraestrutura/Data/ApplicationContext.cs
@@ -0,0 +1,21 @@
+using System;
+using Curso.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Curso.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string strConnection="Data source=(localdb)\\mssqllocaldb; Initial Catalog=DevIO-02;Integrated Security=true;pooling=true;";
+            optionsBuilder
+                .UseSqlServer(strConnection)
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+    }
+}
---
+++ src/7_Infraestrutura/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Curso.Data;
+
+namespace DominandoEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsultarDepartamentos();
+        }
+        static void ConsultarDepartamentos()
+        {
+            using var db = new ApplicationContext();
+
+            var departamentos = db.Departamentos.Where(p => p.Id >= 0).ToArray();
+        }
+    }
+}
