a45c844710b05fc25cdfa52aabcd6be5f3d8b8e9
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-10-02 16:56:41 -0300

    Migraçoes pendentes

--- src/15_Migrations/Curso/src/Program.cs
+++ src/15_Migrations/Curso/src/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Curso.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCore
 {
@@ -6,6 +8,12 @@ namespace EFCore
     {
         static void Main(string[] args)
         {
+            using var db = new ApplicationContext();
+
+            var migracoes = db.Database.GetPendingMigrations();
+            foreach (var migracao in migracoes)
+                Console.WriteLine(migracao);
+
             Console.WriteLine("Hello World!");
         }
     }
