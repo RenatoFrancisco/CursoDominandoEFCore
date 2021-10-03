170c448febebd88901f634a21460a500fd81c2a2
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-10-03 17:23:08 -0300

    Provider PostgreSql

---
+++ src/16_Outros_DBs/Curso/src/Data/ApplicationContenxt.cs
@@ -0,0 +1,32 @@
+using System;
+using Curso.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Curso.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // const string connectionString = "Data source=(localdb)\\mssqllocaldb; Initial Catalog=DevI-O3;Integrated Security=true";
+            const string connectionString = "Host=localhost;Database=DEVIO04;Username=postgres;Password=123";
+
+            optionsBuilder
+                .UseNpgsql(connectionString)
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pessoa>(p => 
+            {
+                p.HasKey(p => p.Id);
+                p.Property(p => p.Nome).HasMaxLength(60).IsUnicode(false);
+            });
+        }
+    }
+}
---
+++ src/16_Outros_DBs/Curso/src/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Curso.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var db = new ApplicationContext();
+
+            db.Database.EnsureCreated();
+            var pessoas = db.Pessoas.ToList();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
