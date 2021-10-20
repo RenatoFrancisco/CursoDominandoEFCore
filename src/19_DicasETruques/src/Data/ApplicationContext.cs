f9bba9416c5d5c1e02b6a3ba766d2635e47965a9
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-10-19 23:18:21 -0300

    Preparando o ambiente

---
+++ src/19_DicasETruques/src/Data/ApplicationContext.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using src.Domain;
+
+namespace src.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Colaborador> Colaboradores { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=Tips;Username=postgres;Password=123");
+            optionsBuilder.LogTo(Console.WriteLine);
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+    }
+}
